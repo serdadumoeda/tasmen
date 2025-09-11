a7db2626640456e4f322adcc91aab92a4d6f387d
serdadumoeda <49605703+serdadumoeda@users.noreply.github.com>
2025-09-11 13:16:45 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -198,7 +198,7 @@
       <div class="logo-container">
         {{-- Pastikan logo ada di public/images/logo-kemnaker.png --}}
         <img src="{{ asset('images/logo-kemnaker.png') }}" alt="Logo Kemnaker">
-        <h1>Gugus Tugas</h1>
+        <h1>TAMASYA</h1>
         <h3>KEMENTERIAN KETENAGAKERJAAN<br>REPUBLIK INDONESIA</h3>
       </div>
     </div>
